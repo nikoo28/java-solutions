4285000156f2a57c5218130c19717d18ac8bbbfb
Nikhil Lohia <nikoo28@gmail.com>
2019-08-10 03:50:31 -0700

    Setup project

---
+++ src/test/java/com/studyalgorithms/javasolutions/AppTest.java
@@ -0,0 +1,20 @@
+package com.studyalgorithms.javasolutions;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
