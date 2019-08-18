7ac9caaa7248f9a859a6892d065ef70e9cf12f89
Nikhil Lohia <nikoo28@gmail.com>
2019-08-17 21:04:17 -0700

    Update with contest questions

---
+++ src/main/java/leetcode/easy/FindWordsThatCanBeFormedByCharacters.java
@@ -0,0 +1,46 @@
+package leetcode.easy;
+
+/**
+ * Created by nikoo28 on 2019-08-17 19:34
+ */
+
+public class FindWordsThatCanBeFormedByCharacters {
+
+  public int countCharacters(String[] words, String chars) {
+
+    int sumOfLengths = 0;
+
+    for (String word : words) {
+      StringBuilder sb = new StringBuilder(chars);
+
+      boolean flag = true;
+      for (int i = 0; i < word.length(); i++) {
+        int idx = sb.indexOf(String.valueOf(word.charAt(i)));
+        if (idx == -1) {
+          flag = false;
+          break;
+        }
+        sb.deleteCharAt(idx);
+      }
+
+      if (flag)
+        sumOfLengths += word.length();
+    }
+
+    return sumOfLengths;
+  }
+
+  public static void main(String[] args) {
+
+    FindWordsThatCanBeFormedByCharacters findWordsThatCanBeFormedByCharacters = new FindWordsThatCanBeFormedByCharacters();
+
+    String[] words = {"hello", "world", "leetcode"};
+    String chars = "welldonehoneyr";
+    System.out.println(findWordsThatCanBeFormedByCharacters.countCharacters(words, chars));
+
+    words = new String[]{"cat", "bt", "hat", "tree"};
+    chars = "atach";
+    System.out.println(findWordsThatCanBeFormedByCharacters.countCharacters(words, chars));
+  }
+
+}
---
+++ src/main/java/leetcode/medium/MaximumLevelSumOfABinaryTree.java
@@ -0,0 +1,68 @@
+package leetcode.medium;
+
+import leetcode.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by nikoo28 on 2019-08-17 19:42
+ */
+
+public class MaximumLevelSumOfABinaryTree {
+
+  public int maxLevelSum(TreeNode root) {
+
+    Queue<TreeNode> levelQueue = new LinkedList<>();
+    int maxSum = 0;
+    levelQueue.add(root);
+    int levels = 0;
+    int maxLevel = 0;
+    while (!levelQueue.isEmpty()) {
+
+      int c = levelQueue.size();
+      int sum = 0;
+      levels++;
+      while (c > 0) {
+
+        TreeNode temp = levelQueue.poll();
+        sum += temp.val;
+        if (temp.left != null) {
+          levelQueue.add(temp.left);
+        }
+        if (temp.right != null) {
+          levelQueue.add(temp.right);
+        }
+        c--;
+      }
+      if (sum > maxSum) {
+        maxLevel = levels;
+        maxSum = sum;
+      }
+    }
+    return maxLevel;
+  }
+
+  public static void main(String[] args) {
+
+    MaximumLevelSumOfABinaryTree maximumLevelSumOfABinaryTree = new MaximumLevelSumOfABinaryTree();
+
+    TreeNode treeNode = new TreeNode(1);
+    treeNode.left = new TreeNode(7);
+    treeNode.right = new TreeNode(0);
+    treeNode.left.left = new TreeNode(7);
+    treeNode.left.right = new TreeNode(-8);
+    treeNode.right.right = new TreeNode(2);
+
+    System.out.println(maximumLevelSumOfABinaryTree.maxLevelSum(treeNode));
+
+    treeNode = new TreeNode(989);
+    treeNode.right = new TreeNode(10250);
+    treeNode.right.left = new TreeNode(98693);
+    treeNode.right.right = new TreeNode(-89388);
+    treeNode.right.right.right = new TreeNode(-32127);
+
+    System.out.println(maximumLevelSumOfABinaryTree.maxLevelSum(treeNode));
+  }
+
+}
