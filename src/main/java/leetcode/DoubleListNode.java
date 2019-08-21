2a05459d4690177454c9876747a8605eae3bd63b
Nikhil Lohia <nikoo28@gmail.com>
2019-08-21 02:41:16 -0700

    Add additonal implementation for LRU cache

---
+++ src/main/java/leetcode/DoubleListNode.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+/**
+ * Created by nikoo28 on 12/17/17 10:41 PM
+ */
+
+public class DoubleListNode {
+  public int key;
+  public int val;
+  public DoubleListNode next;
+  public DoubleListNode prev;
+}
--- src/main/java/leetcode/LRUCache.java
+++ src/main/java/leetcode/LRUCache.java
@@ -8,87 +8,157 @@ import java.util.*;
 
 public class LRUCache {
 
-  Set<Integer> lru;
-  final int maxCapacity;
-  int ticks;
-  HashMap<Integer, Integer> keyValPair;
-  HashMap<Integer, Integer> valTicksPair;
+//  Set<Integer> lru;
+//  final int maxCapacity;
+//  int ticks;
+//  HashMap<Integer, Integer> keyValPair;
+//  HashMap<Integer, Integer> valTicksPair;
+//
+//  public LRUCache(int capacity) {
+//
+//    lru = new HashSet<>();
+//    maxCapacity = capacity;
+//    ticks = 0;
+//    keyValPair = new HashMap<>(capacity);
+//    valTicksPair = new HashMap<>(capacity);
+//  }
+//
+//  public int get(int key) {
+//
+//    if (!lru.contains(key))
+//      return -1;
+//
+//    ticks++;
+//    valTicksPair.put(key, ticks);
+//
+//    return keyValPair.get(key);
+//  }
+//
+//  public void put(int key, int value) {
+//
+//    ticks++;
+//    if (lru.size() < maxCapacity) {
+//      lru.add(key);
+//      keyValPair.put(key, value);
+//      valTicksPair.put(key, ticks);
+//      return;
+//    }
+//
+//    if (lru.contains(key)) {
+//      keyValPair.put(key, value);
+//      valTicksPair.put(key, ticks);
+//      return;
+//    }
+//
+//    int leastUsed = Integer.MIN_VALUE;
+//    int minTicks = Integer.MAX_VALUE;
+//    for (Map.Entry<Integer, Integer> valTicks : valTicksPair.entrySet()) {
+//      if (valTicks.getValue() < minTicks) {
+//        minTicks = valTicks.getValue();
+//        leastUsed = valTicks.getKey();
+//      }
+//    }
+//
+//    lru.remove(leastUsed);
+//    keyValPair.remove(leastUsed);
+//    valTicksPair.remove(leastUsed);
+//
+//    lru.add(key);
+//    keyValPair.put(key, value);
+//    valTicksPair.put(key, ticks);
+//  }
+
+  DoubleListNode head, tail;
+  Map<Integer, DoubleListNode> keyAddressMap;
+  int maxCapacity;
 
   public LRUCache(int capacity) {
-
-    lru = new HashSet<>();
+    keyAddressMap = new HashMap<>(capacity);
     maxCapacity = capacity;
-    ticks = 0;
-    keyValPair = new HashMap<>(capacity);
-    valTicksPair = new HashMap<>(capacity);
+    head = new DoubleListNode();
+    head.prev = null;
+    tail = new DoubleListNode();
+    tail.next = null;
+    head.next = tail;
+    tail.prev = head;
   }
 
   public int get(int key) {
 
-    if (!lru.contains(key))
+    if (!keyAddressMap.containsKey(key))
       return -1;
 
-    ticks++;
-    valTicksPair.put(key, ticks);
+    DoubleListNode node = keyAddressMap.get(key);
+    moveToHead(node);
+    return node.val;
+  }
 
-    return keyValPair.get(key);
+  private void moveToHead(DoubleListNode node) {
+    removeNode(node);
+    addToHead(node);
   }
 
   public void put(int key, int value) {
 
-    ticks++;
-    if (lru.size() < maxCapacity) {
-      lru.add(key);
-      keyValPair.put(key, value);
-      valTicksPair.put(key, ticks);
+    if (keyAddressMap.containsKey(key)) {
+      DoubleListNode node = keyAddressMap.get(key);
+      node.val = value;
+      moveToHead(node);
       return;
     }
 
-    if (lru.contains(key)) {
-      keyValPair.put(key, value);
-      valTicksPair.put(key, ticks);
-      return;
+    DoubleListNode node = new DoubleListNode();
+    node.key = key;
+    node.val = value;
+
+    keyAddressMap.put(key, node);
+    addToHead(node);
+
+    if (keyAddressMap.size() > maxCapacity) {
+      keyAddressMap.remove(tail.prev.key);
+      removeNode(tail.prev);
     }
+  }
 
-    int leastUsed = Integer.MIN_VALUE;
-    int minTicks = Integer.MAX_VALUE;
-    for (Map.Entry<Integer, Integer> valTicks : valTicksPair.entrySet()) {
-      if (valTicks.getValue() < minTicks) {
-        minTicks = valTicks.getValue();
-        leastUsed = valTicks.getKey();
-      }
-    }
+  private void addToHead(DoubleListNode node) {
 
-    lru.remove(leastUsed);
-    keyValPair.remove(leastUsed);
-    valTicksPair.remove(leastUsed);
+    node.prev = head;
+    node.next = head.next;
 
-    lru.add(key);
-    keyValPair.put(key, value);
-    valTicksPair.put(key, ticks);
+    head.next.prev = node;
+    head.next = node;
+  }
+
+  private void removeNode(DoubleListNode node) {
+
+    // If middle node
+    DoubleListNode prevNode = node.prev;
+    DoubleListNode nextNode = node.next;
+    prevNode.next = nextNode;
+    nextNode.prev = prevNode;
   }
 
   public static void main(String[] args) {
 
-    LRUCache cache = new LRUCache( 2 /* capacity */ );
+    LRUCache cache = new LRUCache(2 /* capacity */);
 
-    cache.get(2);
-    cache.put(2,6);
-    cache.get(1);
-    cache.put(1,5);
-    cache.put(1,2);
-    cache.get(1);
-    cache.get(2);
+//    cache.get(2);
+//    cache.put(2,6);
+//    cache.get(1);
+//    cache.put(1,5);
+//    cache.put(1,2);
+//    cache.get(1);
+//    cache.get(2);
 
-//    cache.put(1, 1);
-//    cache.put(2, 2);
-//    cache.get(1);       // returns 1
-//    cache.put(3, 3);    // evicts key 2
-//    cache.get(2);       // returns -1 (not found)
-//    cache.put(4, 4);    // evicts key 1
-//    cache.get(1);       // returns -1 (not found)
-//    cache.get(3);       // returns 3
-//    cache.get(4);       // returns 4
+    cache.put(1, 1);
+    cache.put(2, 2);
+    cache.get(1);       // returns 1
+    cache.put(3, 3);    // evicts key 2
+    cache.get(2);       // returns -1 (not found)
+    cache.put(4, 4);    // evicts key 1
+    cache.get(1);       // returns -1 (not found)
+    cache.get(3);       // returns 3
+    cache.get(4);       // returns 4
 
   }
 
