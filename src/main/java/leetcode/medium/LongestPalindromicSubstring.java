c30ccc86f36b183d80c5a0c920c5636ee554136a
Nikhil Lohia <nikoo28@gmail.com>
2019-08-25 03:26:23 -0700

    Longest palindromic substring

---
+++ src/main/java/leetcode/medium/LongestPalindromicSubstring.java
@@ -0,0 +1,54 @@
+package leetcode.medium;
+
+/**
+ * Created by nikoo28 on 12/18/17 10:14 PM
+ */
+
+public class LongestPalindromicSubstring {
+
+  public String longestPalindrome(String str) {
+
+    if (str.length() <= 1)
+      return str;
+
+    String LPS = "";
+
+    for (int i = 1; i < str.length(); i++) {
+
+      // Consider odd length
+      int low = i;
+      int high = i;
+      while(str.charAt(low) == str.charAt(high)) {
+        low--;
+        high++;
+
+        if (low == -1 || high == str.length())
+          break;
+      }
+
+      String palindrome = str.substring(low+1, high);
+      if (palindrome.length() > LPS.length()) {
+        LPS = palindrome;
+      }
+
+      // Consider even length
+      low = i-1;
+      high = i;
+      while(str.charAt(low) == str.charAt(high)) {
+        low--;
+        high++;
+
+        if (low == -1 || high == str.length())
+          break;
+      }
+
+      palindrome = str.substring(low+1, high);
+      if (palindrome.length() > LPS.length()) {
+        LPS = palindrome;
+      }
+    }
+
+    return LPS;
+  }
+
+}
