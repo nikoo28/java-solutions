81b1b30601864c066e72454692615089c67290aa
Nikhil Lohia <nikoo28@gmail.com>
2019-08-25 14:57:49 -0700

    Crop message in forum Identical pair of indices

---
+++ src/main/java/codility/CroppedMessage.java
@@ -0,0 +1,53 @@
+package codility;
+
+/**
+ * Created by nikoo28 on 2019-08-25 14:21
+ */
+
+public class CroppedMessage {
+
+  public String solution(String message, int K) {
+    // write your code in Java SE 8
+
+    String[] splits = message.split(" ");
+
+    if (splits[0].length() > K) return "";
+
+    if (splits[0].length() == K) return splits[0];
+
+    StringBuilder sb = new StringBuilder(splits[0]);
+    int idx = 1;
+    while (idx < splits.length) {
+      int newLength = sb.length() + 1 + splits[idx].length();
+      if (newLength < K) {
+        sb.append(" ");
+        sb.append(splits[idx]);
+        idx++;
+        continue;
+      }
+
+      if (newLength == K) {
+        sb.append(" ");
+        sb.append(splits[idx]);
+        return sb.toString();
+      }
+
+      return sb.toString();
+    }
+
+    return sb.toString();
+  }
+
+  public static void main(String[] args) {
+
+    CroppedMessage croppedMessage = new CroppedMessage();
+    System.out.println(croppedMessage.solution("Codility We test coders", 14));
+    System.out.println(croppedMessage.solution("Why not", 100));
+    System.out.println(croppedMessage.solution("asda", -1));
+    System.out.println(croppedMessage.solution("The quick brown fox jumps over the lazy dog", 39));
+    System.out.println(croppedMessage.solution("123456789123456789The quick brown fox jumps over the lazy dog", 39));
+    System.out.println(croppedMessage.solution("123456789123456789The quick brown fox jumps over the lazy dog", 21));
+
+  }
+
+}
---
+++ src/main/java/codility/IdenticalPairOfIndices.java
@@ -0,0 +1,104 @@
+package codility;
+
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Created by nikoo28 on 2019-08-25 13:39
+ */
+
+public class IdenticalPairOfIndices {
+
+  public int solution(int[] a) {
+
+//    Integer[] indices = new Integer[a.length];
+//    for (int i = 0; i < a.length; ++i) {
+//      indices[i] = i;
+//    }
+//
+//    Arrays.sort(indices, (i, j) -> {
+//      return Integer.compare(a[i], a[j]);
+//    });
+//
+//    int count = 0;
+//    int idx = 0;
+//    while (idx < indices.length) {
+//      int start = idx;
+//      while (idx < indices.length && a[indices[idx]] == a[indices[start]]) {
+//        idx++;
+//      }
+//
+//      int equalIndices = idx - start;
+//      int possiblePairs = equalIndices * (equalIndices - 1) / 2;
+//
+//      count += possiblePairs;
+//    }
+
+//    return count;
+
+
+    Map<Integer, Long> integerFrequencyMap = new HashMap<>();
+    for (int i : a) {
+      Integer integer = i;
+      integerFrequencyMap.merge(integer, 1L, new BiFunction<Long, Long, Long>() {
+        @Override
+        public Long apply(Long a1, Long b) {
+          return Long.sum(a1, b);
+        }
+      });
+    }
+
+    long count = 0L;
+    for (Long e : integerFrequencyMap.values()) {
+      long l = e * (e - 1) / 2;
+      count += l;
+    }
+
+    if (count > 1000000000)
+        return 1000000000;
+    else
+      return (int) count;
+
+    // Store each pair in map
+//    LinkedHashMap<Integer, ArrayList<Integer>> keyPair = new LinkedHashMap<>();
+//
+//    for (int i = 0; i < A.length; i++) {
+//
+//      if (keyPair.containsKey(A[i])) {
+//        keyPair.get(A[i]).add(i);
+//      } else {
+//        ArrayList<Integer> intList = new ArrayList<>();
+//        intList.add(i);
+//        keyPair.put(A[i], intList);
+//      }
+//    }
+//
+//    // Get pairs
+//    long pairs = 0;
+//    for (Map.Entry<Integer, ArrayList<Integer>> keyPairEntry : keyPair.entrySet()) {
+//      ArrayList<Integer> intList = keyPairEntry.getValue();
+//      for (int i = 1; i < intList.size(); i++) {
+//        pairs = pairs + i;
+//      }
+//      if (pairs >= 1000000000) {
+//        pairs = 1000000000;
+//        break;
+//      }
+//    }
+//
+//    return (int) pairs;
+  }
+
+  public static void main(String[] args) {
+
+    int[] a = {3, 5, 6, 3, 3, 5};
+    IdenticalPairOfIndices identicalPairOfIndices = new IdenticalPairOfIndices();
+
+    System.out.println(identicalPairOfIndices.solution(a));
+
+  }
+
+}
