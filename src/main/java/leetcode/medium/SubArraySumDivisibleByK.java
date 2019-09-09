077164f44cf2ead6d06ccfe9d2afe119ea8f7f78
Nikhil Lohia <nikoo28@gmail.com>
2019-09-08 20:29:04 -0700

    Sub arrays sum divisble by K

---
+++ src/main/java/leetcode/medium/SubArraySumDivisibleByK.java
@@ -0,0 +1,39 @@
+package leetcode.medium;
+
+/**
+ * Created by nikoo28 on 2019-09-08 15:08
+ */
+
+public class SubArraySumDivisibleByK {
+
+  public int subarraysDivByK(int[] A, int K) {
+
+    int subarrays = 0;
+    int[] remainderGroups = new int[K];
+
+    int prefixSum = 0;
+    for (int value : A) {
+      prefixSum += value;
+      int group = prefixSum % K;
+      if (group < 0)
+        group += K;
+      remainderGroups[group]++;
+    }
+
+    for (int remainderCount : remainderGroups) {
+      if (remainderCount > 1)
+        subarrays += (remainderCount * (remainderCount - 1)) / 2.0;
+    }
+
+    return subarrays + remainderGroups[0];
+  }
+
+  public static void main(String[] args) {
+
+    int[] arr = {-1, 2, 9};
+    SubArraySumDivisibleByK subArraySumDivisibleByK = new SubArraySumDivisibleByK();
+
+    System.out.println(subArraySumDivisibleByK.subarraysDivByK(arr, 3));
+  }
+
+}
