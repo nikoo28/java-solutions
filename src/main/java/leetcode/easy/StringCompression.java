0786965625974618d1d9235f39ef953df29905b2
Nikhil Lohia <nikoo28@gmail.com>
2019-09-14 17:34:54 -0700

    String compress

---
+++ src/main/java/leetcode/easy/StringCompression.java
@@ -0,0 +1,25 @@
+package leetcode.easy;
+
+/**
+ * Created by nikoo28 on 2019-09-14 17:34
+ */
+
+public class StringCompression {
+
+  public int compress(char[] chars) {
+    int anchor = 0, write = 0;
+    for (int read = 0; read < chars.length; read++) {
+      if (read + 1 == chars.length || chars[read + 1] != chars[read]) {
+        chars[write++] = chars[anchor];
+        if (read > anchor) {
+          for (char c : ("" + (read - anchor + 1)).toCharArray()) {
+            chars[write++] = c;
+          }
+        }
+        anchor = read + 1;
+      }
+    }
+    return write;
+  }
+
+}
