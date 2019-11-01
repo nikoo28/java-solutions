e80b447173fb7b77fdb944640d57a3c772bb8812
Nikhil Lohia <nikoo28@gmail.com>
2019-11-01 01:35:36 -0700

    Is Symmetric

---
+++ src/main/java/leetcode/easy/SymmetricTree.java
@@ -0,0 +1,32 @@
+package leetcode.easy;
+
+import leetcode.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by nikoo28 on 10/19/19 3:50 PM
+ */
+
+class SymmetricTree {
+
+  boolean isSymmetric(TreeNode root) {
+    Queue<TreeNode> q = new LinkedList<>();
+    q.add(root);
+    q.add(root);
+    while (!q.isEmpty()) {
+      TreeNode t1 = q.poll();
+      TreeNode t2 = q.poll();
+      if (t1 == null && t2 == null) continue;
+      if (t1 == null || t2 == null) return false;
+      if (t1.val != t2.val) return false;
+      q.add(t1.left);
+      q.add(t2.right);
+      q.add(t1.right);
+      q.add(t2.left);
+    }
+    return true;
+  }
+
+}
---
+++ src/test/java/leetcode/easy/SymmetricTreeTest.java
@@ -0,0 +1,39 @@
+package leetcode.easy;
+
+import leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by nikoo28 on 10/19/19 3:52 PM
+ */
+
+class SymmetricTreeTest {
+
+  private SymmetricTree symmetricTree = new SymmetricTree();
+
+  @Test
+  void testIsSymmetric1() {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(2);
+    root.left.left = new TreeNode(3);
+    root.left.right = new TreeNode(4);
+    root.right.left = new TreeNode(4);
+    root.right.right = new TreeNode(3);
+
+    assertTrue(symmetricTree.isSymmetric(root));
+  }
+
+  @Test
+  void testIsSymmetric2() {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(2);
+    root.left.right = new TreeNode(3);
+    root.right.right = new TreeNode(3);
+
+    assertFalse(symmetricTree.isSymmetric(root));
+  }
+}
