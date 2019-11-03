4fa5a3d39dd62a8c4501f42607ddaa5eaf7cc9d1
Nikhil Lohia <nikoo28@gmail.com>
2019-11-02 21:42:14 -0700

    Valid tic tac toe state

---
+++ src/main/java/leetcode/medium/ValidTicTacToeState.java
@@ -0,0 +1,8 @@
+package leetcode.medium;
+
+/**
+ * Created by nikoo28 on 11/2/19 9:36 PM
+ */
+
+public class ValidTicTacToeState {
+}
---
+++ src/test/java/leetcode/medium/ValidTicTacToeStateTest.java
@@ -0,0 +1,16 @@
+package leetcode.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by nikoo28 on 11/2/19 9:38 PM
+ */
+
+class ValidTicTacToeStateTest {
+
+  @Test
+  void validTicTacToe() {
+  }
+}
