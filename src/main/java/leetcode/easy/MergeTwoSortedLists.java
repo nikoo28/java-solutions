f078661912910c681fbf4270f497249dff56852d
Nikhil Lohia <nikoo28@gmail.com>
2020-06-15 22:38:08 -0700

    Merge 2 sorted linked lists

---
+++ src/main/java/leetcode/easy/MergeTwoSortedLists.java
@@ -0,0 +1,44 @@
+package leetcode.easy;
+
+import leetcode.ListNode;
+
+/**
+ * Created by nikoo28 on 6/15/20 10:23 PM
+ */
+
+public class MergeTwoSortedLists {
+
+  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+    // Create a sentinal/dummy node to start
+    ListNode returnNode = new ListNode(Integer.MIN_VALUE);
+
+    // Create a copy of this node to iterate while solving the problem
+    ListNode headNode = returnNode;
+
+    // Traverse till one of the list reaches the end
+    while (l1 != null && l2 != null) {
+
+      // Compare the 2 values of lists
+      if (l1.val <= l2.val) {
+        returnNode.next = l1;
+        l1 = l1.next;
+      } else {
+        returnNode.next = l2;
+        l2 = l2.next;
+      }
+      returnNode = returnNode.next;
+    }
+
+    // Append the remaining list
+    if (l1 == null) {
+      returnNode.next = l2;
+    } else if (l2 == null) {
+      returnNode.next = l1;
+    }
+
+    // return the next node to sentinal node
+    return headNode.next;
+  }
+
+}
---
+++ src/test/java/leetcode/easy/MergeTwoSortedListsTest.java
@@ -0,0 +1,48 @@
+package leetcode.easy;
+
+import leetcode.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by nikoo28 on 6/15/20 10:32 PM
+ */
+
+class MergeTwoSortedListsTest {
+
+  MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
+
+  @Test
+  void testMergeTwoLists1() {
+    ListNode l1 = new ListNode(1);
+    l1.next = new ListNode(2);
+    l1.next.next = new ListNode(4);
+
+    ListNode l2 = new ListNode(1);
+    l2.next = new ListNode(3);
+    l2.next.next = new ListNode(4);
+
+    ListNode listNode = mergeTwoSortedLists.mergeTwoLists(l1, l2);
+    assertEquals(1, listNode.val);
+    assertEquals(1, listNode.next.val);
+    assertEquals(2, listNode.next.next.val);
+    assertEquals(3, listNode.next.next.next.val);
+    assertEquals(4, listNode.next.next.next.next.val);
+    assertEquals(4, listNode.next.next.next.next.next.val);
+  }
+
+  @Test
+  void testMergeTwoLists2() {
+    ListNode l1 = null;
+
+    ListNode l2 = new ListNode(2);
+    l2.next = new ListNode(3);
+    l2.next.next = new ListNode(4);
+
+    ListNode listNode = mergeTwoSortedLists.mergeTwoLists(l1, l2);
+    assertEquals(2, listNode.val);
+    assertEquals(3, listNode.next.val);
+    assertEquals(4, listNode.next.next.val);
+  }
+}
