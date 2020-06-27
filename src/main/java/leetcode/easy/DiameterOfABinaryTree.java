8bde4a13132cfb1a0c3a68d34e78701bc608b457
Nikhil Lohia <nikoo28@gmail.com>
2020-06-27 02:45:12 -0700

    Diameter of a binary tree

---
+++ src/main/java/leetcode/easy/DiameterOfABinaryTree.java
@@ -0,0 +1,49 @@
+package leetcode.easy;
+
+import leetcode.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Created by nikoo28 on 6/27/20 2:30 AM
+ */
+
+public class DiameterOfABinaryTree {
+
+  public int diameterOfBinaryTree(TreeNode root) {
+
+    Map<TreeNode, Integer> map = new HashMap<>();
+    Stack<TreeNode> stack = new Stack<>();
+    int diameter = 0;
+
+    if (root != null)
+      stack.push(root);
+
+    while (!stack.isEmpty()) {
+      TreeNode node = stack.peek();
+
+      // Fill up stack to perform post-order traversal
+      if (node.left != null && !map.containsKey(node.left)) {
+        stack.push(node.left);
+      } else if (node.right != null && !map.containsKey(node.right)) {
+        stack.push(node.right);
+      } else {
+
+        // Process the root, once left and right sub-tree have been processed
+        stack.pop();
+        int leftDepth = map.getOrDefault(node.left, 0);
+        int rightDepth = map.getOrDefault(node.right, 0);
+
+        // Put the max depth at a node in the map
+        map.put(node, 1 + Math.max(leftDepth, rightDepth));
+
+        // Update the max diameter found so far
+        diameter = Math.max(diameter, leftDepth + rightDepth);
+      }
+    }
+    return diameter;
+  }
+
+}
---
+++ src/test/java/leetcode/easy/DiameterOfABinaryTreeTest.java
@@ -0,0 +1,60 @@
+package leetcode.easy;
+
+import leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by nikoo28 on 6/27/20 2:33 AM
+ */
+
+class DiameterOfABinaryTreeTest {
+
+  DiameterOfABinaryTree diameterOfABinaryTree = new DiameterOfABinaryTree();
+
+  @Test
+  void testDiameterOfBinaryTree1() {
+
+    //
+    //       1
+    //      / \
+    //     2   3
+    //    / \
+    //   4   5
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.left.left = new TreeNode(4);
+    root.left.right = new TreeNode(5);
+    root.right = new TreeNode(3);
+
+    assertEquals(3, diameterOfABinaryTree.diameterOfBinaryTree(root));
+  }
+
+  @Test
+  void testDiameterOfBinaryTree2() {
+
+    //           1
+    //         /   \
+    //       4      3
+    //     /  \      \
+    //   2    5       8
+    //      /        /
+    //     6        7
+    //            /  \
+    //           0    9
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(4);
+    root.left.left = new TreeNode(2);
+    root.left.right = new TreeNode(5);
+    root.left.right.left = new TreeNode(6);
+
+    root.right = new TreeNode(3);
+    root.right.right = new TreeNode(8);
+    root.right.right.left = new TreeNode(7);
+    root.right.right.left.left = new TreeNode(0);
+    root.right.right.left.right = new TreeNode(9);
+
+    assertEquals(7, diameterOfABinaryTree.diameterOfBinaryTree(root));
+  }
+}
