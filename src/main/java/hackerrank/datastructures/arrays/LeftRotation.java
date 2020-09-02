0c8b6652afeaff4eb5540da661d58fe200ebc4b7
Nikhil Lohia <nikoo28@gmail.com>
2020-09-02 00:58:30 -0700

    Add HackerRank LeftRotation with test cases

---
+++ src/main/java/hackerrank/datastructures/arrays/LeftRotation.java
@@ -0,0 +1,66 @@
+package hackerrank.datastructures.arrays;
+
+/**
+ * Created by nikoo28 on 9/2/20 12:05 AM
+ */
+
+public class LeftRotation {
+
+  // Helper function to reverse an array from start index to end index
+  public void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+      int temp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = temp;
+      start++;
+      end--;
+    }
+  }
+
+  public void rotateLeftUsingReverse(int[] arr, int k) {
+    k %= arr.length;
+
+    // Reverse all numbers
+    reverse(arr, 0, arr.length - 1);
+
+    // Reverse first arr.length-k numbers
+    reverse(arr, 0, arr.length - k - 1);
+
+    // Reverse last k numbers
+    reverse(arr, arr.length - k, arr.length - 1);
+  }
+
+  public void rotateLeftUsingBruteForce(int[] arr, int k) {
+    k %= arr.length;
+
+    // Rotate the array k times
+    for (int i = 0; i < k; i++) {
+      int j, temp;
+      temp = arr[0];
+      // Perform rotation for each iteration
+      for (j = 0; j < arr.length - 1; j++)
+        arr[j] = arr[j + 1];
+      arr[j] = temp;
+    }
+  }
+
+  public void rotateLeft(int[] arr, int k) {
+    k %= arr.length;
+    int[] temp = new int[k];
+
+    // Store the first k elements in a temp array
+    for (int i = 0; i < temp.length; i++) {
+      temp[i] = arr[i];
+    }
+
+    // Shift rest of the arr[]
+    for (int i = 0; i < arr.length - k; i++) {
+      arr[i] = arr[i + k];
+    }
+
+    // Store back the k elements
+    for (int i = arr.length - k; i < arr.length; i++) {
+      arr[i] = temp[i - k - 1];
+    }
+  }
+}
---
+++ src/test/java/hackerrank/datastructures/arrays/LeftRotationTest.java
@@ -0,0 +1,68 @@
+package hackerrank.datastructures.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by nikoo28 on 9/2/20 12:17 AM
+ */
+
+class LeftRotationTest {
+
+  LeftRotation leftRotation = new LeftRotation();
+
+  @Test
+  void rotateLeft1() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int rotation = 2;
+    int result[] = {3, 4, 5, 1, 2};
+
+    leftRotation.rotateLeftUsingReverse(arr, rotation);
+    assertArrayEquals(result, arr);
+
+    arr = new int[]{1, 2, 3, 4, 5};
+    leftRotation.rotateLeftUsingBruteForce(arr, rotation);
+    assertArrayEquals(result, arr);
+
+    arr = new int[]{1, 2, 3, 4, 5};
+    leftRotation.rotateLeft(arr, rotation);
+    assertArrayEquals(result, arr);
+  }
+
+  @Test
+  void rotateLeft2() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int rotation = 22;
+    int result[] = {3, 4, 5, 1, 2};
+
+    leftRotation.rotateLeftUsingReverse(arr, rotation);
+    assertArrayEquals(result, arr);
+
+    arr = new int[]{1, 2, 3, 4, 5};
+    leftRotation.rotateLeftUsingBruteForce(arr, rotation);
+    assertArrayEquals(result, arr);
+
+    arr = new int[]{1, 2, 3, 4, 5};
+    leftRotation.rotateLeft(arr, rotation);
+    assertArrayEquals(result, arr);
+  }
+
+  @Test
+  void rotateLeft3() {
+    int arr[] = {1};
+    int rotation = 22;
+    int result[] = {1};
+
+    leftRotation.rotateLeftUsingReverse(arr, rotation);
+    assertArrayEquals(result, arr);
+
+    arr = new int[]{1};
+    leftRotation.rotateLeftUsingBruteForce(arr, rotation);
+    assertArrayEquals(result, arr);
+
+    arr = new int[]{1};
+    leftRotation.rotateLeft(arr, rotation);
+    assertArrayEquals(result, arr);
+  }
+}
