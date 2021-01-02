246e1a3303e838f2b5cb075a6e685f0cbbe2a480
Nikhil Lohia <nikoo28@gmail.com>
2021-01-01 18:45:02 -0800

    Two Sum

--- src/main/java/leetcode/easy/TwoSum.java
+++ src/main/java/leetcode/easy/TwoSum.java
@@ -8,14 +8,64 @@ import java.util.*;
 
 class TwoSum {
 
-  int[] twoSum(int[] nums, int target) {
+  int[] twoSumBruteForce(int[] nums, int target) {
 
-    Map<Integer, Integer> map = new HashMap<>();
     for (int i = 0; i < nums.length; i++) {
+      for (int j = 0; j < nums.length; j++) {
+        if (nums[i] + nums[j] == target)
+          return new int[]{i, j};
+      }
+    }
+    throw new IllegalArgumentException("No two sum solution");
+  }
+
+  int[] twoSumSorting(int[] nums, int target) {
+    int[] copyArray = Arrays.copyOf(nums, nums.length);
+    Arrays.sort(copyArray);
+
+    int head = 0;
+    int tail = copyArray.length - 1;
+    int num1 = 0, num2 = 0;
+    while (head < tail) {
+      int sum = copyArray[head] + copyArray[tail];
+      if (sum < target) {
+        head++;
+      }
+      else if (sum > target) {
+        tail--;
+      } else {
+        num1 = copyArray[head];
+        num2 = copyArray[tail];
+        break;
+      }
+    }
+
+    int[] result = new int[2];
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == num1)
+        result[0] = i;
+      if (nums[i] == num2)
+        result[1] = i;
+    }
+    return result;
+  }
+
+  int[] twoSumHashing(int[] nums, int target) {
+
+    // Create a HashMap
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < nums.length; i++) {
+
+      // Get the complement using the target value
       int complement = target - nums[i];
+
+      // Search the hashmap for complement, if found, we got our pair
       if (map.containsKey(complement)) {
         return new int[]{map.get(complement), i};
       }
+
+      // Put the element in hashmap for subsequent searches.
       map.put(nums[i], i);
     }
     throw new IllegalArgumentException("No two sum solution");
--- src/test/java/leetcode/easy/TwoSumTest.java
+++ src/test/java/leetcode/easy/TwoSumTest.java
@@ -13,11 +13,32 @@ class TwoSumTest {
   private TwoSum twoSum = new TwoSum();
 
   @Test
-  void twoSum() {
+  void twoSum1() {
     int[] nums = {2, 7, 11, 15};
     int target = 9;
 
-    int[] res = twoSum.twoSum(nums, target);
-    assertEquals(9, nums[res[0]] + nums[res[1]]);
+    int[] res = twoSum.twoSumHashing(nums, target);
+    assertEquals(target, nums[res[0]] + nums[res[1]]);
+
+    res = twoSum.twoSumBruteForce(nums, target);
+    assertEquals(target, nums[res[0]] + nums[res[1]]);
+
+    res = twoSum.twoSumSorting(nums, target);
+    assertEquals(target, nums[res[0]] + nums[res[1]]);
+  }
+
+  @Test
+  void twoSum2() {
+    int[] nums = {3, 2, 4};
+    int target = 6;
+
+    int[] res = twoSum.twoSumHashing(nums, target);
+    assertEquals(target, nums[res[0]] + nums[res[1]]);
+
+    res = twoSum.twoSumBruteForce(nums, target);
+    assertEquals(target, nums[res[0]] + nums[res[1]]);
+
+    res = twoSum.twoSumSorting(nums, target);
+    assertEquals(target, nums[res[0]] + nums[res[1]]);
   }
 }
