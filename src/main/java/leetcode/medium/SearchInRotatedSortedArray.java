1c6f073ae6c5a19a67d0b8c91173533145b92ee4
Nikhil Lohia <nikoo28@gmail.com>
2021-01-23 17:04:35 -0800

    Search in Rotated Sorted Array

--- src/main/java/leetcode/medium/SearchInRotatedSortedArray.java
+++ src/main/java/leetcode/medium/SearchInRotatedSortedArray.java
@@ -44,6 +44,41 @@ class SearchInRotatedSortedArray {
     return -1;
   }
 
+  private int modifiedBinarySearch(int[] arr, int target, int left, int right) {
+
+    // Not found
+    if (left > right)
+      return -1;
+
+    // Avoid overflow, same as (left + right)/2
+    int mid = left + ((right - left) / 2);
+    if (arr[mid] == target)
+      return mid; // Found
+
+    // If left half is sorted
+    if (arr[mid] >= arr[left]) {
+
+      // If key is in left half
+      if (arr[left] <= target && target <= arr[mid])
+        return modifiedBinarySearch(arr, target, left, mid - 1);
+      else
+        return modifiedBinarySearch(arr, target, mid + 1, right);
+
+    } else {
+      // If right half is sorted
+
+      // If key is in right half
+      if (arr[mid] <= target && target <= arr[right])
+        return modifiedBinarySearch(arr, target, mid + 1, right);
+      else
+        return modifiedBinarySearch(arr, target, left, mid - 1);
+    }
+  }
+
+  /**
+   * Search by first finding the rotation index. The index about which the array has been rotated.
+   * Then compare the target value to do a simple binary search in the left sub-array or right sub-array.
+   */
   public int search(int[] nums, int target) {
 
     int rotationIndex = findRotationIndex(nums);
@@ -55,5 +90,13 @@ class SearchInRotatedSortedArray {
       return binarySearch(nums, 0, rotationIndex, target);
     } else
       return binarySearch(nums, rotationIndex + 1, nums.length - 1, target);
+  }
+
+  /**
+   * Modified version of binary search. One sub-array will always be sorted. Based on that we can always discard one
+   * half of the array
+   */
+  public int alternateSearch(int[] nums, int target) {
+    return modifiedBinarySearch(nums, target, 0, nums.length - 1);
   }
 }
--- src/test/java/leetcode/medium/SearchInRotatedSortedArrayTest.java
+++ src/test/java/leetcode/medium/SearchInRotatedSortedArrayTest.java
@@ -17,36 +17,46 @@ class SearchInRotatedSortedArrayTest {
   }
 
   @Test
-  void testSearch() {
+  void testSearchFound() {
     int[] nums = {8, 4, 5, 6, 7};
     assertEquals(1, searchInRotatedSortedArray.search(nums, 4));
+    assertEquals(1, searchInRotatedSortedArray.alternateSearch(nums, 4));
 
     nums = new int[]{4, 5, 6, 7};
     assertEquals(0, searchInRotatedSortedArray.search(nums, 4));
+    assertEquals(0, searchInRotatedSortedArray.alternateSearch(nums, 4));
 
     nums = new int[]{4, 5, 6, 7, 1};
     assertEquals(0, searchInRotatedSortedArray.search(nums, 4));
-
-    nums = new int[]{};
-    assertEquals(-1, searchInRotatedSortedArray.search(nums, 4));
+    assertEquals(0, searchInRotatedSortedArray.alternateSearch(nums, 4));
 
     nums = new int[]{4, 5, 6, 7, 0, 1, 2};
     assertEquals(4, searchInRotatedSortedArray.search(nums, 0));
-
-    nums = new int[]{4, 5, 6, 7, 0, 1, 2};
-    assertEquals(-1, searchInRotatedSortedArray.search(nums, 3));
+    assertEquals(4, searchInRotatedSortedArray.alternateSearch(nums, 0));
 
     nums = new int[]{4};
     assertEquals(0, searchInRotatedSortedArray.search(nums, 4));
-
-    nums = new int[]{4};
-    assertEquals(-1, searchInRotatedSortedArray.search(nums, 0));
+    assertEquals(0, searchInRotatedSortedArray.alternateSearch(nums, 4));
 
     nums = new int[]{5, 1, 3};
     assertEquals(0, searchInRotatedSortedArray.search(nums, 5));
+    assertEquals(0, searchInRotatedSortedArray.alternateSearch(nums, 5));
 
     nums = new int[]{8, 9, 2, 3, 4};
     assertEquals(1, searchInRotatedSortedArray.search(nums, 9));
+    assertEquals(1, searchInRotatedSortedArray.alternateSearch(nums, 9));
+  }
+
+  @Test
+  void testSearchNotFound() {
+    int[] nums = new int[]{};
+    assertEquals(-1, searchInRotatedSortedArray.alternateSearch(nums, 4));
+
+    nums = new int[]{4, 5, 6, 7, 0, 1, 2};
+    assertEquals(-1, searchInRotatedSortedArray.alternateSearch(nums, 3));
+
+    nums = new int[]{4};
+    assertEquals(-1, searchInRotatedSortedArray.alternateSearch(nums, 0));
   }
 
 }
