0d1151102f27d7668157199ffac7ddeef42cf506
Nikhil Lohia <nikoo28@gmail.com>
2021-02-21 21:16:35 -0800

    Add sample code for linked list

--- src/main/java/leetcode/ListNode.java
+++ src/main/java/leetcode/ListNode.java
@@ -11,4 +11,24 @@ public class ListNode {
   public ListNode(int x) {
     val = x;
   }
+
+  public static void main(String[] args) {
+
+    // Create 3 nodes
+    ListNode l1 = new ListNode(4);
+    ListNode l2 = new ListNode(8);
+    ListNode l3 = new ListNode(15);
+
+    // Link the nodes
+    l1.next = l2;
+    l2.next = l3;
+    l3.next = null;
+
+    // Print values
+    ListNode ptr = l1;
+    while(ptr != null) {
+      System.out.println(ptr.val);
+      ptr = ptr.next;
+    }
+  }
 }
