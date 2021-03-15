6beabdbb40850afd15592481ad3aff0ee62e6569
Nikhil Lohia <nikoo28@gmail.com>
2021-03-14 19:12:58 -0700

    Number line jumps

---
+++ src/main/java/hackerrank/algorithms/implementation/NumberLineJumps.java
@@ -0,0 +1,20 @@
+package hackerrank.algorithms.implementation;
+
+/**
+ * Created by nikoo28 on 3/14/21 6:30 PM
+ */
+
+public class NumberLineJumps {
+
+  String kangaroo(int x1, int v1, int x2, int v2) {
+
+    if (v1 < v2)
+      return "NO";
+
+    if ((x2 - x1) % (v1 - v2) == 0)
+      return "YES";
+    else
+      return "NO";
+  }
+
+}
---
+++ src/test/java/hackerrank/algorithms/implementation/NumberLineJumpsTest.java
@@ -0,0 +1,48 @@
+package hackerrank.algorithms.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by nikoo28 on 3/14/21 7:07 PM
+ */
+
+class NumberLineJumpsTest {
+
+  private final NumberLineJumps numberLineJumps;
+
+  public NumberLineJumpsTest() {
+    numberLineJumps = new NumberLineJumps();
+  }
+
+  @Test
+  void testNumberLineJumps1(){
+    int x1 = 0;
+    int v1 = 2;
+    int x2 = 5;
+    int v2 = 3;
+
+    assertEquals("NO", numberLineJumps.kangaroo(x1, v1, x2, v2));
+  }
+
+  @Test
+  void testNumberLineJumps2(){
+    int x1 = 0;
+    int v1 = 3;
+    int x2 = 4;
+    int v2 = 2;
+
+    assertEquals("YES", numberLineJumps.kangaroo(x1, v1, x2, v2));
+  }
+
+  @Test
+  void testNumberLineJumps3(){
+    int x1 = 14;
+    int v1 = 4;
+    int x2 = 98;
+    int v2 = 2;
+
+    assertEquals("YES", numberLineJumps.kangaroo(x1, v1, x2, v2));
+  }
+}
