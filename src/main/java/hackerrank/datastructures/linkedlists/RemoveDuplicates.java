5e453c86fc4ee9a7010690ca7b124e2dc1a173a0
Nikhil Lohia <nlohia@godaddy.com>
2021-06-17 18:23:14 +0530

    Remove Duplicates in a sorted linked list

---
+++ src/main/java/hackerrank/datastructures/linkedlists/RemoveDuplicates.java
@@ -0,0 +1,27 @@
+package hackerrank.datastructures.linkedlists;
+
+import leetcode.ListNode;
+
+public class RemoveDuplicates {
+
+  ListNode removeDuplicates(ListNode head) {
+
+    ListNode prev = head;
+    ListNode temp = prev.next;
+
+    while (temp != null) {
+      if (temp.val == prev.val) {
+        temp = temp.next;
+        continue;
+      }
+
+      prev.next = temp;
+      prev = temp;
+      temp = temp.next;
+    }
+
+    prev.next = null;
+    return head;
+  }
+
+}
---
+++ src/test/java/hackerrank/datastructures/linkedlists/RemoveDuplicatesTest.java
@@ -0,0 +1,75 @@
+package hackerrank.datastructures.linkedlists;
+
+import leetcode.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveDuplicatesTest {
+
+  private final RemoveDuplicates removeDuplicates;
+
+  public RemoveDuplicatesTest() {
+    removeDuplicates = new RemoveDuplicates();
+  }
+
+  @Test
+  void testRemoveDuplicates1() {
+    ListNode head = new ListNode(1);
+    head.next = new ListNode(2);
+    head.next.next = new ListNode(2);
+    head.next.next.next = new ListNode(2);
+    head.next.next.next.next = new ListNode(3);
+    head.next.next.next.next.next = new ListNode(4);
+    head.next.next.next.next.next.next = new ListNode(4);
+    head.next.next.next.next.next.next.next = new ListNode(5);
+
+    removeDuplicates.removeDuplicates(head);
+
+    assertEquals(1, head.val);
+    assertEquals(2, head.next.val);
+    assertEquals(3, head.next.next.val);
+    assertEquals(4, head.next.next.next.val);
+    assertEquals(5, head.next.next.next.next.val);
+    assertNull(head.next.next.next.next.next);
+  }
+
+  @Test
+  void testRemoveDuplicates2() {
+    ListNode head = new ListNode(1);
+    head.next = new ListNode(1);
+
+    removeDuplicates.removeDuplicates(head);
+
+    assertEquals(1, head.val);
+    assertNull(head.next);
+  }
+
+  @Test
+  void testRemoveDuplicates3() {
+    ListNode head = new ListNode(1);
+    head.next = new ListNode(1);
+    head.next.next = new ListNode(1);
+
+    removeDuplicates.removeDuplicates(head);
+
+    assertEquals(1, head.val);
+    assertNull(head.next);
+  }
+
+  @Test
+  void testRemoveDuplicates4() {
+    ListNode head = new ListNode(1);
+    head.next = new ListNode(1);
+    head.next.next = new ListNode(2);
+    head.next.next.next = new ListNode(3);
+    head.next.next.next.next = new ListNode(3);
+
+    removeDuplicates.removeDuplicates(head);
+
+    assertEquals(1, head.val);
+    assertEquals(2, head.next.val);
+    assertEquals(3, head.next.next.val);
+    assertNull(head.next.next.next);
+  }
+}
