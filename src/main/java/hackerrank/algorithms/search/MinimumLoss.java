89b97d21d0c418383725dbb9bf203fa6cb6290ef
Nikhil Lohia <nlohia@godaddy.com>
2021-06-23 15:16:02 +0530

    Making the code concise

--- src/main/java/hackerrank/algorithms/search/MinimumLoss.java
+++ src/main/java/hackerrank/algorithms/search/MinimumLoss.java
@@ -1,37 +1,33 @@
 package hackerrank.algorithms.search;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MinimumLoss {
 
   public int minimumLoss(List<Long> prices) {
 
-    // Copy all elements to an array
-    long[] pricesArray = new long[prices.size()];
-
     // Index all the elements
-    Map<Long, Integer> priceDayNumberMap = new HashMap<>();
+    Map<Long, Integer> priceIndexMap = new HashMap<>();
     for (int i = 0; i < prices.size(); i++) {
-      pricesArray[i] = prices.get(i);
-      priceDayNumberMap.put(prices.get(i), i);
+      priceIndexMap.put(prices.get(i), i);
     }
 
-    Arrays.sort(pricesArray);
-    Long minimumLoss = Long.MAX_VALUE;
+    Collections.sort(prices);
+    long minimumLoss = Long.MAX_VALUE;
 
     // Start from the end
     for (int i = prices.size() - 1; i > 0 ; i--) {
+
       // Reject the invalid values 💣
-      if (priceDayNumberMap.get(pricesArray[i]) > priceDayNumberMap.get(pricesArray[i-1]))
+      if (priceIndexMap.get(prices.get(i)) >
+          priceIndexMap.get(prices.get(i-1)))
         continue;
-      long dayLoss = pricesArray[i] - pricesArray[i-1];
+
+      long dayLoss = prices.get(i) - prices.get(i-1);
       minimumLoss = Math.min(dayLoss, minimumLoss);
     }
 
-    return minimumLoss.intValue();
+    return (int) minimumLoss;
   }
 
 }
