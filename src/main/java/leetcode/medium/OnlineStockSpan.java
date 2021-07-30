403fde9b19d19f7f25865fcf6d2880de1ad3a21f
Nikhil Lohia <nlohia@godaddy.com>
2021-07-30 14:01:53 +0530

    Online Stock Span

---
+++ src/main/java/leetcode/medium/OnlineStockSpan.java
@@ -0,0 +1,54 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class OnlineStockSpan {
+
+  List<Integer> list;
+
+  public OnlineStockSpan() {
+    this.list = new ArrayList<>();
+  }
+
+  public int next(int price) {
+    list.add(price);
+    int count = 0;
+    for (int i = list.size() - 1; i >= 0; i--) {
+      if (list.get(i) > price)
+        break;
+      count++;
+    }
+    return count;
+  }
+
+  public int[] calculateSpans(int[] prices) {
+
+    int[] spans = new int[prices.length];
+    spans[0] = 1; // Span of first element is always 1
+
+    Stack<Integer> indexStack = new Stack<>();
+
+    // Push the index of first element
+    indexStack.push(0);
+
+    for (int i = 1; i < prices.length; i++) {
+      while (!indexStack.isEmpty()
+          && prices[indexStack.peek()] <= prices[i])
+        indexStack.pop();
+
+      // If index stack is empty, the price at index 'i'
+      // is greater than all previous values
+      if (indexStack.isEmpty())
+        spans[i] = i + 1;
+      else
+        spans[i] = i - indexStack.peek();
+
+      indexStack.push(i);
+    }
+
+    return spans;
+  }
+
+}
---
+++ src/test/java/leetcode/medium/OnlineStockSpanTest.java
@@ -0,0 +1,59 @@
+package leetcode.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OnlineStockSpanTest {
+
+  private OnlineStockSpan onlineStockSpan;
+
+  public OnlineStockSpanTest() {
+    onlineStockSpan = new OnlineStockSpan();
+  }
+
+  @Test
+  void testCalculateSpans1() {
+    int[] prices = {100, 80, 60, 70, 60, 75, 85};
+    int[] spans = {1, 1, 1, 2, 1, 4, 6};
+
+    int[] actualSpans = onlineStockSpan.calculateSpans(prices);
+    assertArrayEquals(spans, actualSpans);
+  }
+
+  @Test
+  void testCalculateSpans2() {
+    int[] prices = {100, 60, 70, 65, 80, 85, 200};
+    int[] spans = {1, 1, 2, 1, 4, 5, 7};
+
+    int[] actualSpans = onlineStockSpan.calculateSpans(prices);
+    assertArrayEquals(spans, actualSpans);
+  }
+
+  @Test
+  void testCalculateSpans3() {
+    int[] prices = {100};
+    int[] spans = {1};
+
+    int[] actualSpans = onlineStockSpan.calculateSpans(prices);
+    assertArrayEquals(spans, actualSpans);
+  }
+
+  @Test
+  void testCalculateSpans4() {
+    int[] prices = {100, 90, 80, 70, 60, 50, 40};
+    int[] spans = {1, 1, 1, 1, 1, 1, 1};
+
+    int[] actualSpans = onlineStockSpan.calculateSpans(prices);
+    assertArrayEquals(spans, actualSpans);
+  }
+
+  @Test
+  void testCalculateSpans5() {
+    int[] prices = {100, 110, 120, 130, 140, 150, 160};
+    int[] spans = {1, 2, 3, 4, 5, 6, 7};
+
+    int[] actualSpans = onlineStockSpan.calculateSpans(prices);
+    assertArrayEquals(spans, actualSpans);
+  }
+}
