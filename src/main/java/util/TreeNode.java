c72faa8f65788a683a25b24124735cfde9b29096
Nikhil Lohia <nlohia@godaddy.com>
2021-10-21 00:31:49 -0700

    Improve TreeNode util

--- src/main/java/util/TreeNode.java
+++ src/main/java/util/TreeNode.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Definition for a binary tree node.
  */
@@ -15,25 +18,137 @@ public class TreeNode {
     right = null;
   }
 
+  public static void inOrderTraversal(TreeNode root) {
+
+    // Terminating condition
+    if (root == null)
+      return;
+
+    // Traverse the left node
+    inOrderTraversal(root.left);
+
+    // Print the root node
+    System.out.print(root.val + " -> ");
+
+    // Go to the right node
+    inOrderTraversal(root.right);
+  }
+
+  public static void preOrderTraversal(TreeNode root) {
+
+    // Terminating condition
+    if (root == null)
+      return;
+
+    // Print the root node
+    System.out.print(root.val + " -> ");
+
+    // Traverse the left node
+    preOrderTraversal(root.left);
+
+    // Go to the right node
+    preOrderTraversal(root.right);
+  }
+
+  public static void postOrderTraversal(TreeNode root) {
+
+    // Terminating condition
+    if (root == null)
+      return;
+
+    // Traverse the left node
+    postOrderTraversal(root.left);
+
+    // Go to the right node
+    postOrderTraversal(root.right);
+
+    // Print the root node
+    System.out.print(root.val + " -> ");
+  }
+
+  public static void levelOrderTraversal(TreeNode root) {
+
+    Queue<TreeNode> treeNodeQueue = new LinkedList<>();
+
+    // Start with the first/root node
+    treeNodeQueue.add(root);
+
+    // Run a loop till this queue is not empty
+    while (!treeNodeQueue.isEmpty()) {
+      TreeNode treeNode = treeNodeQueue.poll();
+
+      // Print the value
+      System.out.print(treeNode.val + " -> ");
+
+      // Add left child to queue
+      if (treeNode.left != null)
+        treeNodeQueue.add(treeNode.left);
+
+      // Add right child to queue
+      if (treeNode.right != null)
+        treeNodeQueue.add(treeNode.right);
+    }
+  }
+
   public static void main(String[] args) {
 
-    TreeNode root = new TreeNode(4);
+    // Creating a tree that looks like
+    //            2
+    TreeNode root = new TreeNode(2);
 
+    // Adding left and right child
+    //            2
+    //          /  \
+    //        8     4
     root.left = new TreeNode(8);
-    root.right = new TreeNode(15);
+    root.right = new TreeNode(4);
 
     TreeNode leftNode = root.left;
     TreeNode rightNode = root.right;
 
-    leftNode.left = new TreeNode(16);
+    // Adding more child to left node
+    //            2
+    //          /  \
+    //        8     4
+    //      /  \
+    //     3   7
+    leftNode.left = new TreeNode(3);
+    leftNode.right = new TreeNode(7);
 
-    rightNode.left = new TreeNode(23);
-    rightNode.right = new TreeNode(42);
+    // Adding child to right node
+    //            2
+    //          /  \
+    //        8     4
+    //      /  \     \
+    //     3   7      1
+    rightNode.right = new TreeNode(1);
 
-    // Will print 4
+    // Adding child to child of right node
+    //            2
+    //          /  \
+    //        8     4
+    //      /  \     \
+    //     3   7      1
+    //               /
+    //              6
+    rightNode.right.left = new TreeNode(6);
+
+    // Will print 2
     System.out.println(root.val);
 
-    // Will print 23
-    System.out.println(root.right.left.val);
+    // Will print 6
+    System.out.println(root.right.right.left.val);
+
+    System.out.println("\nIn-order traversal: ");
+    inOrderTraversal(root);
+
+    System.out.println("\nPre-order traversal: ");
+    preOrderTraversal(root);
+
+    System.out.println("\nPost-order traversal: ");
+    postOrderTraversal(root);
+
+    System.out.println("\nLevel order traversal: ");
+    levelOrderTraversal(root);
   }
 }
---
+++ src/main/java/util/WriteToFile.java
@@ -0,0 +1,52 @@
+package util;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.stream.IntStream;
+
+public class WriteToFile {
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+
+    String logFile = args[0].split("=")[1];
+    int numberOfRecordsPerSecond = Integer.parseInt(args[1].split("=")[1]);
+    int maximumRecords = Integer.parseInt(args[2].split("=")[1]);
+
+    System.out.println("Number of records per second: " + numberOfRecordsPerSecond);
+    System.out.println("Maximum count: " + maximumRecords);
+    System.out.println("Flushing to: " + logFile);
+
+    RateLimiter rateLimiter = RateLimiter.create(numberOfRecordsPerSecond);
+    BufferedWriter writer = new BufferedWriter(new FileWriter(logFile));
+    System.out.println("\nProgress:");
+
+    IntStream.range(0, maximumRecords).forEach(i -> {
+      try {
+        rateLimiter.acquire();
+        writer.append(getPayload(i));
+        writer.append("\n");
+        writer.flush();
+        System.out.print(i);
+        System.out.print("\r");
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    });
+
+    writer.close();
+  }
+
+  private static String getPayload(int count) {
+    String timestamp = new Timestamp(System.currentTimeMillis()).toInstant().toString();
+    return "{" +
+        "\"event_timestamp\":\"" + timestamp + "\"," +
+        "\"idx\":\"" + count + "\"," +
+        "\"key\":\"value" + count + "\"" +
+        "}";
+  }
+
+}
