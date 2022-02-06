db336931ca789310058e003601a3a0a0abe8789c
Nikhil Lohia <nlohia@godaddy.com>
2022-02-06 12:27:08 -0800

    Move Zeroes

--- src/test/java/leetcode/easy/MoveZeroesTest.java
+++ src/test/java/leetcode/easy/MoveZeroesTest.java
@@ -6,14 +6,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MoveZeroesTest {
 
-  MoveZeroes moveZeroes = new MoveZeroes();
+  private final MoveZeroes moveZeroes;
+
+  MoveZeroesTest() {
+    moveZeroes = new MoveZeroes();
+  }
 
   @Test
-  void moveZeroes() {
+  void testMoveZeroes1() {
     int[] arr = {0, 1, 0, 3, 12};
     int[] expectedResult = {1, 3, 12, 0, 0};
 
     moveZeroes.moveZeroes(arr);
     assertArrayEquals(expectedResult, arr);
+  }
+
+  @Test
+  void testMoveZeroes2() {
+    int[] arr = {0, 0, 0};
+    int[] expected = {0, 0, 0};
+
+    moveZeroes.moveZeroes(arr);
+    assertArrayEquals(expected, arr);
+  }
+
+  @Test
+  void testMoveZeroes3() {
+    int[] arr = {1, 2, 3};
+    int[] expected = {1, 2, 3};
+
+    moveZeroes.moveZeroes(arr);
+    assertArrayEquals(expected, arr);
   }
 }
