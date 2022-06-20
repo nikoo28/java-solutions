f5de1f49f9a01e2175e7a41ad6115e5e6b297f56
Nikhil Lohia <nlohia@godaddy.com>
2022-06-19 23:53:37 -0700

    Top K frequent elements

---
+++ src/main/java/leetcode/medium/TopKFrequentElements.java
@@ -0,0 +1,40 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TopKFrequentElements {
+
+  public int[] topKFrequent(int[] nums, int k) {
+
+    List<Integer>[] bucket = new List[nums.length + 1];
+    Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+    for (int n : nums) {
+      frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+    }
+
+    for (int key : frequencyMap.keySet()) {
+      int frequency = frequencyMap.get(key);
+      if (bucket[frequency] == null) {
+        bucket[frequency] = new ArrayList<>();
+      }
+      bucket[frequency].add(key);
+    }
+
+    int[] res = new int[k];
+    int counter = 0;
+
+    for (int pos = bucket.length - 1; pos >= 0 && counter < k; pos--) {
+      if (bucket[pos] != null) {
+        for (Integer integer : bucket[pos]) {
+          res[counter++] = integer;
+        }
+      }
+    }
+    return res;
+  }
+
+}
---
+++ src/test/java/leetcode/medium/TopKFrequentElementsTest.java
@@ -0,0 +1,41 @@
+package leetcode.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TopKFrequentElementsTest {
+
+  private final TopKFrequentElements topKFrequentElements;
+
+  TopKFrequentElementsTest() {
+    topKFrequentElements = new TopKFrequentElements();
+  }
+
+  @Test
+  void testTopKFrequent1() {
+    int[] arr = {1, 1, 1, 2, 2, 3};
+    int k = 2;
+
+    int[] res = {1, 2};
+    assertArrayEquals(res, topKFrequentElements.topKFrequent(arr, k));
+  }
+
+  @Test
+  void testTopKFrequent2() {
+    int[] arr = {1, 1, 2};
+    int k = 1;
+
+    int[] res = {1};
+    assertArrayEquals(res, topKFrequentElements.topKFrequent(arr, k));
+  }
+
+  @Test
+  void testTopKFrequent3() {
+    int[] arr = {4, 1, -1, 2, -1, 2, 3};
+    int k = 2;
+
+    int[] res = {-1, 2};
+    assertArrayEquals(res, topKFrequentElements.topKFrequent(arr, k));
+  }
+}
