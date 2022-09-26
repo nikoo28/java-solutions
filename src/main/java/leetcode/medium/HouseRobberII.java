e9ac2690ac38847bf969fbeb68d315e622c522a8
Nikhil Lohia <nikoo28@gmail.com>
2022-09-25 17:24:10 -0700

    Fix build error

--- src/main/java/leetcode/medium/HouseRobberII.java
+++ src/main/java/leetcode/medium/HouseRobberII.java
@@ -25,7 +25,20 @@ public class HouseRobberII {
   }
 
   private int robHelper(int[] nums) {
-    // Same code from HouseRobber
+
+    if (nums.length < 2)
+      return nums[0];
+
+    int[] dp = new int[nums.length];
+
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+
+    for (int i = 2; i < nums.length; i++) {
+      dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+    }
+
+    return dp[nums.length - 1];
   }
 
 }
