ba7c7b55d7984e186a328bd7505bc572d54ca60b
A Shreya Sri <84087589+shreyasri258@users.noreply.github.com>
2023-01-04 07:17:00 +0530

    Update IsomorphicStrings.java

--- src/main/java/leetcode/easy/IsomorphicStrings.java
+++ src/main/java/leetcode/easy/IsomorphicStrings.java
@@ -3,8 +3,8 @@
 Two strings s and t are isomorphic if the characters in s can be replaced to get t.
 
 All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.*/
-
-class Solution {
+package leetcode.easy;
+Public class IsomorphicStrings {
     public boolean isIsomorphic(String s, String t) {
          Map m = new HashMap();
     for (Integer i=0; i<s.length(); ++i)
