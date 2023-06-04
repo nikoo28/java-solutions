f9bd991c59a738c9f3ffc87b576221cb7d3d6f8c
Nikhil Lohia <nikoo28@gmail.com>
2023-06-04 20:58:03 +0530

    Isomorphic strings

---
+++ src/main/java/leetcode/medium/IsomorphicStrings.java
@@ -0,0 +1,38 @@
+package leetcode.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicStrings {
+
+  boolean isIsomorphic(String s, String t) {
+
+    if (s.length() != t.length()) {
+      return false;
+    }
+
+    // Create a hashmap to store character mappings
+    Map<Character, Character> charMappingMap = new HashMap<>();
+
+    for (int i = 0; i < s.length(); i++) {
+
+      char original = s.charAt(i);
+      char replacement = t.charAt(i);
+
+      if (!charMappingMap.containsKey(original)) {
+        if (!charMappingMap.containsValue(replacement))
+          charMappingMap.put(original, replacement);
+        else
+          return false;
+      }
+      else {
+        char mappedCharacter = charMappingMap.get(original);
+        if (mappedCharacter != replacement)
+          return false;
+      }
+    }
+
+    return true;
+  }
+
+}
---
+++ src/test/java/leetcode/medium/IsomorphicStringsTest.java
@@ -0,0 +1,46 @@
+package leetcode.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IsomorphicStringsTest {
+
+  private final IsomorphicStrings isomorphicStrings;
+
+  IsomorphicStringsTest() {
+    isomorphicStrings = new IsomorphicStrings();
+  }
+
+  @Test
+  void testIsIsomorphic1() {
+    String s = "badc";
+    String t = "baba";
+
+    assertFalse(isomorphicStrings.isIsomorphic(s, t));
+  }
+
+  @Test
+  void testIsIsomorphic2() {
+    String s = "add";
+    String t = "egg";
+
+    assertTrue(isomorphicStrings.isIsomorphic(s, t));
+  }
+
+  @Test
+  void testIsIsomorphic3() {
+    String s = "foo";
+    String t = "bar";
+
+    assertFalse(isomorphicStrings.isIsomorphic(s, t));
+  }
+
+  @Test
+  void testIsIsomorphic4() {
+    String s = "paper";
+    String t = "title";
+
+    assertTrue(isomorphicStrings.isIsomorphic(s, t));
+  }
+}
