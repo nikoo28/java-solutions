a5c6f7ae090cb3ca7b3a98b441dddf19bf76698d
Nikhil Lohia <nikoo28@gmail.com>
2023-10-01 00:54:05 -0700

    Valid Palindrome

--- src/main/java/leetcode/easy/ValidPalindromeII.java
+++ src/main/java/leetcode/easy/ValidPalindromeII.java
@@ -1,2 +1,34 @@
-package leetcode.easy;public class ValidPalindromeII {
+package leetcode.easy;
+
+public class ValidPalindromeII {
+
+  boolean validPalindrome(String s) {
+    int left = 0;
+    int right = s.length() - 1;
+
+    while (left < right) {
+      if (s.charAt(left) == s.charAt(right)) {
+        left++;
+        right--;
+      } else {
+        return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+      }
+    }
+
+    return true;
+  }
+
+  private boolean isPalindrome(String s, int left, int right) {
+    while (left < right) {
+      if (s.charAt(left) == s.charAt(right)) {
+        left++;
+        right--;
+      } else {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
 }
