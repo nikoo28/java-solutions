6e0a9061913b795c35c6e808ae346e664714e475
Nikhil Lohia <nikoo28@gmail.com>
2024-04-12 11:46:58 -0700

    Updated test case

--- src/test/java/leetcode/easy/MajorityElementTest.java
+++ src/test/java/leetcode/easy/MajorityElementTest.java
@@ -23,4 +23,10 @@ class MajorityElementTest {
     int[] num = {2, 2, 1, 3, 1, 2, 2};
     assertEquals(2, majorityElement.majorityElement(num));
   }
+
+  @Test
+  void testMajorityElement3() {
+    int[] num = {6, 5, 5};
+    assertEquals(5, majorityElement.majorityElement(num));
+  }
 }
