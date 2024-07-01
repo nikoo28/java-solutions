98de8a0fd52e505c8bc4c6d240f3e84b7c33d97b
Nikhil Lohia <nikoo28@gmail.com>
2024-06-30 18:57:46 -0700

    MinCost Climbing Stairs

--- src/main/java/leetcode/easy/MinCostClimbingStairs.java
+++ src/main/java/leetcode/easy/MinCostClimbingStairs.java
@@ -6,14 +6,20 @@ package leetcode.easy;
 
 class MinCostClimbingStairs {
 
-  public int minCostClimbingStairs(int[] cost) {
-    int f1 = 0, f2 = 0;
-    for (int i = cost.length - 1; i >= 0; --i) {
-      int f0 = cost[i] + Math.min(f1, f2);
-      f2 = f1;
-      f1 = f0;
+  int minCostClimbingStairs(int[] cost) {
+
+    int n = cost.length;
+    int[] minCost = new int[n + 1];
+
+    for (int i = 2; i <= n; i++) {
+      minCost[i] =
+          Math.min(
+              (cost[i - 1] + minCost[i - 1]),
+              (cost[i - 2] + minCost[i - 2])
+          );
     }
-    return Math.min(f1, f2);
+
+    return minCost[n];
   }
 
 }
